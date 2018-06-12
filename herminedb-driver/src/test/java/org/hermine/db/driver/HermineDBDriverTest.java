51264e30f56665a35a845162f8af868783714292
pull-vert <frederic.montariol@gmail.com>
2018-06-13 00:06:40 +0200

    Add AssertJ for assertions in Tests, Disable forName test that doesn't work with IntelliJ, Run test

--- herminedb-driver/src/test/java/org/hermine/db/driver/HermineDBDriverTest.java
+++ herminedb-driver/src/test/java/org/hermine/db/driver/HermineDBDriverTest.java
@@ -16,26 +16,59 @@
  */
 package org.hermine.db.driver;
 
+import jdk.incubator.sql2.DataSource;
 import jdk.incubator.sql2.DataSourceFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HermineDBDriverTest {
+
+    private static final String URL = "<database connect string>";
+    private static final String USER = "<database user name>";
+    private static final String PASSWORD = "<database user password>";
+    // Define this to be the most trivial SELECT possible
+//    private static final String TRIVIAL = "SELECT 1 FROM DUAL";
 
     private static final String FACTORY_NAME = "org.hermine.db.driver.HermineDataSourceFactory";
 
     private static final Logger logger = LogManager.getLogger(HermineDBDriverTest.class);
 
-    /**
-     * Verify that DataSourceFactory.forName works. Can't do anything without that.
-     */
     @Test
+    @Disabled
+    @DisplayName("Verify that DataSourceFactory.forName works. Can't do anything without that.")
     public void forName() {
-        assertEquals("org.hermine.db.driver.HermineDataSourceFactory",
-                DataSourceFactory.forName(FACTORY_NAME).getClass().getName());
-        logger.info("DataSourceFactory.forName class = {}", DataSourceFactory.forName(FACTORY_NAME).getClass().getName());
+        // When
+        var name = DataSourceFactory.forName(FACTORY_NAME).getClass().getName();
+
+        // Then
+        assertThat(name)
+                .as("Verify that the DataSourceFactory found is a HermineDataSourceFactory")
+                .isEqualTo("org.hermine.db.driver.HermineDataSourceFactory");
+
+        logger.info("DataSourceFactory.forName class = {}", name);
+    }
+
+    @Test
+    @DisplayName("Verify that can create a DataSource, though not a Connection. Should work even if there is no database.")
+    public void createDataSource() {
+        // Given
+        DataSourceFactory factory = new HermineDataSourceFactory();
+
+        // When
+        DataSource ds = factory.builder()
+                .url(URL)
+                .username(USER)
+                .password(PASSWORD)
+                .build();
+
+        // Then
+        assertThat(ds)
+                .as("Verify that DataSource is created.")
+                .isNotNull();
     }
 }
